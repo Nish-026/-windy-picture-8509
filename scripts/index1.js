e34d83db1e16599b472cb2b1bb4be361b0a01cd4
Nish-026 <nishtha.kashyap2608@gmail.com>
2023-02-01 20:23:30 +0530

    Fixed Nav Bar

---
+++ scripts/index1.js
@@ -0,0 +1,7 @@
+let close_coupon_button = document.getElementById("close-coupon");
+let coupon = document.getElementById("coupon");
+
+
+close_coupon_button.addEventListener("click", () => {
+    coupon.style.display = "none";
+})
